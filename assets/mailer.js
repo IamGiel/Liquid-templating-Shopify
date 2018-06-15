dd6bd47e75dc02a468f897ecb0c53a562e7712de
Server <november14giel@gmail.com>
2018-06-14 23:36:25 -0400

    added email forwarding service

--- assets/mailer.js
+++ assets/mailer.js
@@ -3,7 +3,7 @@
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
-  to: "test@example.com",
+  to: process.env.EMAIL,
   from: "test@example.com",
   subject: "Sending with SendGrid is Fun",
   text: "and easy to do anywhere, even with Node.js",
